54b467efbad979765fb2ac6d8e9b95585a419873
Kahina Ouali <kahinaouali.pro@gmail.com>
2024-05-10 18:51:40 +0200

    Mise en page différente pour home

--- assets/js/theme.js
+++
@@ -1,6 +0,0 @@
-const themeToggleBtn = document.getElementById('theme-toggle');
-
-themeToggleBtn.addEventListener('click', () => {
-    document.body.classList.toggle('theme-light'); 
-    document.body.classList.toggle('theme-dark');
-});
